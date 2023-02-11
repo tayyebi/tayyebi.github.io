1b5afdb2809b5a3487d4953374cad27c0e10cf57
tayyebi <tayyebimohammadreza@gmail.com>
2023-02-11 23:36:03 +0330

    Added some comments.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,43 @@
-// var biops = document.getElementsByClassName("bionic");
-// for (var i = 0; i < biops.length; i++) {
-//     var text = biops[i].innerHTML;
-//     var pattern = /([a-zA-Z\u0600-\u06FF\uFB8A\u067E\u0686\u06AF‌]+)/g;
-//     text = text.replace(pattern, "<span style=\"color:red\">$1</span>");
-//     biops[i].innerHTML = text;
-// }
-
-
 // Get all bionic paragraphs
 var biops = document.getElementsByClassName("bionic");
 // Foreach bionic
 for (var i = 0; i < biops.length; i++) {
     // Get content and convert to array
-    var indeces = [];
     var content = biops[i].innerHTML.split('');
-    // Foreach charachter in the content
+    // Array to hold seperators addresses
+    var indeces = [];
+    // Check if we are inside an HTML tag
     var tag = false;
+    // Foreach charachter in the content
     for (var j = 0; j < content.length; j++) {
+        // If a tag starts
         if (content[j] == '<') {
             tag = true;
-        } else if (content[j] == '>') {
+        }
+        // If a tag ends
+        else if (content[j] == '>') {
             tag = false;
-        } else if (content[j] == ' ' && !tag) {
+        }
+        // We seperate words with \s space!
+        else if (content[j] == ' ' && !tag) {
             indeces.push(j);
         }
     }
+    // Remove indeces if there are a serial of them
     for (var j = indeces.length; j > 0; j--) {
         if (indeces[j] - indeces[j - 1] == 1)
             indeces.splice(j, 1);
     }
-    console.log(indeces);
+    // Add spans to each word
     for (var j = indeces.length; j > 0; j--) {
         span_b =
+            // If wasn't first char
             (j > 1 ? "</span>" : "") +
+            // If wasn't last char
             (j < indeces.length ? "<span class=\"b\">" : "");
+        // Add span to the right place
         content.splice(indeces[j - 1], 0, span_b);
     }
-    console.log(content.join(''));
+    // Replace new content with previous
     biops[i].innerHTML = content.join('');
 }
